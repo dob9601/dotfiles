322f9d2d3b93a2cf7174985bbadf89a8a5fae080
Jointhedots Sync <jtd@danielobr.ie>
2022-04-13 10:34:47 +0100

    🔁 Sync fish dotfile

--- config.fish
+++ config.fish
@@ -1,3 +1,9 @@
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'edc/bass'
+fundle plugin 'franciscolourenco/done'
+fundle plugin 'jethrokuan/z'
+
 alias vim='nvim'
 # alias cat='bat'
 alias ls='exa --long --header'
@@ -6,7 +12,8 @@ alias cat='bat'
 export GPG_TTY=(tty)
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-set theme_color_scheme nord
+bass source ~/repos/git-subrepo/.rc
+
 
 # function fish_prompt
 #     powerline-shell --shell bare $status
@@ -16,9 +23,13 @@ set theme_color_scheme nord
     #neofetch --separator '\t'
 #end
 
-function fish_title
-    set -q argv[1]; or set argv fish
-    echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
-end
+set -g theme_color_scheme nord
+set -g theme_title_display_process no
+
+# function fish_title
+#     set -q argv[1]; or set argv fish
+#     echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
+# end
 
 export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
