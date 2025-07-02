bf9d67b5934e98915aa3a1dd721ba3413f1866f9
Dan O'Brien <dob9601@github.com>
2025-07-02 11:07:41 +0100

    Add script to install helix editor

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+export HELIX_VERSION="25.01.1"
+export HELIX_CONFIG_DIR="$HOME/.config/helix"
+
+################# INSTALL HELIX #################
+curl -sL -o /tmp/helix.tar.xz https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/helix-${HELIX_VERSION}-x86_64-linux.tar.xz
+
+mkdir -p $HELIX_CONFIG_DIR
+
+tar -xvf /tmp/helix.tar.xz -C /tmp/helix --strip-components=1
+
+mv /tmp/helix/runtime $HELIX_CONFIG_DIR/runtime
+mv /tmp/helix /usr/local/bin/hx
+#################################################
