e68175ca8858f46d333d1efecf84fdf566ee9d43
Dan O'Brien <dob9601@github.com>
2025-07-31 12:34:59 +0100

    Disable helix install for now

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ export HELIX_VERSION="25.01.1"
 export HELIX_CONFIG_DIR="$HOME/.config/helix"
 
 ################# INSTALL HELIX #################
-curl -sL -o /tmp/helix.tar.xz https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/helix-${HELIX_VERSION}-x86_64-linux.tar.xz
+# curl -sL -o /tmp/helix.tar.xz https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/helix-${HELIX_VERSION}-x86_64-linux.tar.xz
 
-mkdir -p $HELIX_CONFIG_DIR
+# mkdir -p $HELIX_CONFIG_DIR
 
-mkdir /tmp/helix
-tar -xvf /tmp/helix.tar.xz -C /tmp/helix --strip-components=1
+# mkdir - /tmp/helix
+# tar -xvf /tmp/helix.tar.xz -C /tmp/helix --strip-components=1
 
-mv /tmp/helix/runtime $HELIX_CONFIG_DIR/runtime
-mv /tmp/helix /usr/local/bin/hx
+# mv /tmp/helix/runtime $HELIX_CONFIG_DIR/runtime
+# mv /tmp/helix /usr/local/bin/hx
 
-ln -s ./helix/config.toml $HELIX_CONFIG_DIR
-ln -s ./helix/languages.toml $HELIX_CONFIG_DIR
+# ln -s ./helix/config.toml $HELIX_CONFIG_DIR
+# ln -s ./helix/languages.toml $HELIX_CONFIG_DIR
 
 
 ################## INSTALL ZSH ##################
@@ -26,4 +26,4 @@ if [ "$SHELL" != "/usr/bin/zsh" ]; then
     exec zsh -il
 fi;
 
-ln -s ./.zshrc "$HOME/.zshrc" 
+ln -s ./.zshrc "$HOME/.zshrc"
