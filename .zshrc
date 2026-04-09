4989c65034969d9ad222f58cd3077e8258e64465
Dan O'Brien <dob9601@github.com>
2026-04-09 14:41:41 +0100

    Fix git identity, add starship prompt

--- .zshrc
+++ .zshrc
@@ -1,4 +1,4 @@
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
 
-[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+eval "$(starship init zsh)"
--- install.sh
+++ install.sh
@@ -2,28 +2,21 @@
 
 set -eux -o pipefail
 
-# Helix
-# export HELIX_VERSION="25.01.1"
-# export HELIX_CONFIG_DIR="$HOME/.config/helix"
-
-# curl -sL -o /tmp/helix.tar.xz https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/helix-${HELIX_VERSION}-x86_64-linux.tar.xz
-
-# mkdir -p $HELIX_CONFIG_DIR
-
-# mkdir - /tmp/helix
-# tar -xvf /tmp/helix.tar.xz -C /tmp/helix --strip-components=1
-
-# mv /tmp/helix/runtime $HELIX_CONFIG_DIR/runtime
-# mv /tmp/helix /usr/local/bin/hx
-
-# ln -s ./helix/config.toml $HELIX_CONFIG_DIR
-# ln -s ./helix/languages.toml $HELIX_CONFIG_DIR
+mkdir -p "$HOME/.config"
 
 # Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sudo chsh -s $(which zsh) $(whoami)
-
+rm ~/.zshrc
 ln -s ./.zshrc "$HOME/.zshrc"
 
+## Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo chsh -s "$(which zsh)" "$(whoami)"
+
+## Starship
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
+starship preset gruvbox-rainbow -o ~/.config/starship.toml
+
 # Git
-ln -s ./.gitconfig "$HOME/.gitconfig"
+git config --global user.name "Dan O'Brien"
+git config --global user.email "dob9601@github.com"
+git config --global core.editor vim
