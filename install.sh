73feedad8d98e3afc1cc7b4dde81639927fec0f5
Dan O'Brien <dob9601@gmail.com>
2026-05-05 15:43:08 +0100

    Comment out zsh-autocomplete git clone command
    Comment out the zsh-autocomplete plugin installation line.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ rm -f "$HOME/.zshrc"
 ln -s .dotfiles/.zshrc "$HOME/.zshrc"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autocomplete"
+# git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autocomplete"
 
 ## Starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir ~/.local/bin
